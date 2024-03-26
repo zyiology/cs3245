76c539747db2f1f3febedcc76f7ff42e8365249d
zhiyi <e1332814@u.nus.edu.sg>
2024-03-26 11:54:26 +0800

    testing hw2 algo

--- test/test.py
+++ test/test.py
@@ -1,4 +1,16 @@
-import nltk
+def process_parentheses(lst):
+    stack = [[]]  # Start with a list in the stack to collect the processed items
+    for item in lst:
+        if item == '(':
+            stack.append([])  # Start a new sublist when seeing '('
+        elif item == ')':
+            last_list = stack.pop()  # End the current sublist
+            stack[-1].append(last_list)  # Add the ended sublist to the previous list
+        else:
+            stack[-1].append(item)  # Add the item to the current sublist
+    return stack[0]  # Return the first item in the stack, which is the processed list
 
-print('imported nltk')
-print(nltk.__version__)
+# Example usage
+lst = ["a", "b", "(", "c", "(", "d", ")", ")"]
+result = process_parentheses(lst)
+print(result)
